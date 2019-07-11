321c32456676bc85a048472d63be590f3aff070a
amerllica <amerllica@gmail.com>
2019-07-11 14:54:16 +0430

    Update webpack, set new optimization based on new version of webpack and Fix linter issues.

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,5 +1,5 @@
+/* eslint-disable sort-keys-fix/sort-keys-fix */
 const path = require('path');
-const webpack = require('webpack');
 
 const assetsDirName = 'assets';
 const outputImagesDirName = 'img';
@@ -9,118 +9,126 @@ const distDir = path.join(__dirname, assetsDirName);
 const srcDir = path.join(__dirname, './src');
 
 module.exports = [
-    {
-        name: 'client',
-        target: 'web',
-        entry: {
-            client: `${srcDir}/client.jsx`,
-            vendor: ['react', 'react-dom', 'react-helmet', 'react-router-dom'],
-        },
-        output: {
-            path: distDir,
-            filename: 'js/[name].js',
-            publicPath: distDir,
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            alias: {
-                '~': srcDir,
-            },
-        },
-        devtool: 'source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /(node_modules\/)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        }
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|svg)$/,
-                    exclude: /node_modules/,
-                    loader: 'file-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[name].[ext]',
-                        publicPath: `/${assetsDirName}/${outputFontsDirName}`,
-                        outputPath: outputFontsDirName,
-                    }
-                },
-                {
-                    test: /\.(jpg|png)$/,
-                    exclude: /node_modules/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[name].[ext]',
-                        publicPath: `/${assetsDirName}/${outputImagesDirName}`,
-                        outputPath: outputImagesDirName,
-                    }
-                }
-            ],
-        },
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({
-                name:'vendor',
-                filename: 'js/[name].js',
-                minChunks: Infinity,
-            }),
-        ]
+  {
+    name: 'client',
+    target: 'web',
+    mode: 'development',
+    entry: {
+      client: `${srcDir}/client.jsx`,
+      vendor: ['react', 'react-dom', 'react-helmet', 'react-router-dom'],
     },
-    {
-        name: 'server',
-        target: 'node',
-        entry: `${srcDir}/server.jsx`,
-        output: {
-            path: distDir,
-            filename: 'js/server.js',
-            libraryTarget: 'commonjs2',
-            publicPath: distDir,
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            alias: {
-                '~': srcDir,
+    output: {
+      path: distDir,
+      filename: 'js/[name].js',
+      publicPath: distDir,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '~': srcDir,
+      },
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules\/)/,
+          use: [
+            {
+              loader: 'babel-loader',
             },
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /(node_modules\/)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        }
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|svg)$/,
-                    exclude: /node_modules/,
-                    loader: 'file-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[name].[ext]',
-                        publicPath: `/${assetsDirName}/${outputFontsDirName}`,
-                        outputPath: outputFontsDirName,
-                    }
-                },
-                {
-                    test: /\.(jpg|png)$/,
-                    exclude: /node_modules/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[name].[ext]',
-                        publicPath: `/${assetsDirName}/${outputImagesDirName}`,
-                        outputPath: outputImagesDirName,
-                    }
-                }
-            ],
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            limit: 1024,
+            name: '[name].[ext]',
+            publicPath: `/${assetsDirName}/${outputFontsDirName}`,
+            outputPath: outputFontsDirName,
+          },
         },
-    }
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            name: '[name].[ext]',
+            publicPath: `/${assetsDirName}/${outputImagesDirName}`,
+            outputPath: outputImagesDirName,
+          },
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        name: 'vendor',
+        cacheGroups: {
+          vendors: {
+            test: 'vendor',
+            filename: 'js/[name].js',
+            enforce: true,
+            chunks: 'all',
+          },
+        },
+      },
+    },
+  },
+  {
+    name: 'server',
+    target: 'node',
+    mode: 'development',
+    entry: `${srcDir}/server.jsx`,
+    output: {
+      path: distDir,
+      filename: 'js/server.js',
+      libraryTarget: 'commonjs2',
+      publicPath: distDir,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '~': srcDir,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules\/)/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            limit: 1024,
+            name: '[name].[ext]',
+            publicPath: `/${assetsDirName}/${outputFontsDirName}`,
+            outputPath: outputFontsDirName,
+          },
+        },
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            name: '[name].[ext]',
+            publicPath: `/${assetsDirName}/${outputImagesDirName}`,
+            outputPath: outputImagesDirName,
+          },
+        },
+      ],
+    },
+  },
 ];
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys-fix/sort-keys-fix */
 const path = require('path');
 const webpack = require('webpack');
 const StatsPlugin = require('stats-webpack-plugin');
@@ -12,148 +13,178 @@ const distDir = path.join(__dirname, assetsDirName);
 const srcDir = path.join(__dirname, './src');
 
 module.exports = [
-    {
-        name: 'client',
-        target: 'web',
-        entry: {
-            client: `${srcDir}/client.jsx`,
-            vendor: ['react', 'react-dom', 'react-helmet', 'react-router-dom'],
-        },
-        output: {
-            path: distDir,
-            filename: 'js/[name].js',
-            publicPath: distDir,
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            alias: {
-                '~': srcDir,
-            },
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /(node_modules\/)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        }
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|svg)$/,
-                    exclude: /node_modules/,
-                    loader: 'file-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[hash:base64:5].[ext]',
-                        publicPath: `/${assetsDirName}/${outputFontsDirName}`,
-                        outputPath: outputFontsDirName,
-                    }
-                },
-                {
-                    test: /\.(jpg|png)$/,
-                    exclude: /node_modules/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[hash:base64:5].[ext]',
-                        publicPath: `/${assetsDirName}/${outputImagesDirName}`,
-                        outputPath: outputImagesDirName,
-                    }
-                }
-            ],
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: '"production"'
-                }
-            }),
-            new UglifyJsPlugin({
-                uglifyOptions: {
-                    compress: {
-                        warnings: false,
-                        drop_console: true,
-                        drop_debugger: true
-                    },
-                    parse: {
-                        shebang: true,
-                    },
-                    output: {
-                        comments: false,
-                        beautify: false,
-                    }
-                }
-            }),
-            new webpack.optimize.CommonsChunkPlugin({
-                name: 'vendor',
-                filename: 'js/[name].js',
-                minChunks: Infinity,
-            }),
-            new webpack.optimize.OccurrenceOrderPlugin(),
-        ]
+  {
+    name: 'client',
+    target: 'web',
+    mode: 'production',
+    entry: {
+      client: `${srcDir}/client.jsx`,
+      vendor: ['react', 'react-dom', 'react-helmet', 'react-router-dom'],
     },
-    {
-        name: 'server',
-        target: 'node',
-        entry: `${srcDir}/server.jsx`,
-        output: {
-            path: distDir,
-            filename: 'js/server.js',
-            libraryTarget: 'commonjs2',
-            publicPath: distDir,
-        },
-        resolve: {
-            extensions: ['.js', '.jsx'],
-            alias: {
-                '~': srcDir,
+    output: {
+      path: distDir,
+      filename: 'js/[name].js',
+      publicPath: distDir,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '~': srcDir,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules\/)/,
+          use: [
+            {
+              loader: 'babel-loader',
             },
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /(node_modules\/)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        }
-                    ]
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|svg)$/,
-                    exclude: /node_modules/,
-                    loader: 'file-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[hash:base64:5].[ext]',
-                        publicPath: `/${assetsDirName}/${outputFontsDirName}`,
-                        outputPath: outputFontsDirName,
-                    }
-                },
-                {
-                    test: /\.(jpg|png)$/,
-                    exclude: /node_modules/,
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024,
-                        name: '[hash:base64:5].[ext]',
-                        publicPath: `/${assetsDirName}/${outputImagesDirName}`,
-                        outputPath: outputImagesDirName,
-                    }
-                }
-            ],
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            limit: 1024,
+            name: '[hash:base64:5].[ext]',
+            publicPath: `/${assetsDirName}/${outputFontsDirName}`,
+            outputPath: outputFontsDirName,
+          },
         },
-        plugins: [
-            new StatsPlugin('stats.json', {
-                chunkModules: true,
-                modules: true,
-                chunks: true,
-                exclude: [/node_modules[\\\/]react/],
-            }),
-        ]
-    }
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            name: '[hash:base64:5].[ext]',
+            publicPath: `/${assetsDirName}/${outputImagesDirName}`,
+            outputPath: outputImagesDirName,
+          },
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        name: 'vendor',
+        cacheGroups: {
+          vendors: {
+            test: 'vendor',
+            filename: 'js/[name].js',
+            enforce: true,
+            chunks: 'all',
+          },
+        },
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"',
+        },
+      }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            warnings: false,
+            drop_console: true,
+            drop_debugger: true,
+          },
+          parse: {
+            shebang: true,
+          },
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        },
+      }),
+      new webpack.optimize.OccurrenceOrderPlugin(undefined),
+    ],
+  },
+  {
+    name: 'server',
+    target: 'node',
+    mode: 'production',
+    entry: `${srcDir}/server.jsx`,
+    output: {
+      path: distDir,
+      filename: 'js/server.js',
+      libraryTarget: 'commonjs2',
+      publicPath: distDir,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        '~': srcDir,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules\/)/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|svg)$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            limit: 1024,
+            name: '[hash:base64:5].[ext]',
+            publicPath: `/${assetsDirName}/${outputFontsDirName}`,
+            outputPath: outputFontsDirName,
+          },
+        },
+        {
+          test: /\.(jpg|png)$/,
+          exclude: /node_modules/,
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            name: '[hash:base64:5].[ext]',
+            publicPath: `/${assetsDirName}/${outputImagesDirName}`,
+            outputPath: outputImagesDirName,
+          },
+        },
+      ],
+    },
+    plugins: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: {
+            warnings: false,
+            drop_console: true,
+            drop_debugger: true,
+          },
+          parse: {
+            shebang: true,
+          },
+          output: {
+            comments: false,
+            beautify: false,
+          },
+        },
+      }),
+      new StatsPlugin(
+        'stats.json',
+        {
+          chunkModules: true,
+          modules: true,
+          chunks: true,
+          exclude: [/node_modules[\\/]react/],
+        },
+        undefined
+      ),
+    ],
+  },
 ];
