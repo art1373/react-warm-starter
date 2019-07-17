2a67d278d6b6b3e0629fd92edcd6b8480d604819
amerllica <amerllica@gmail.com>
2019-07-17 22:13:46 +0430

    Folding for better human readability

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,6 +1,10 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
+/* Material Components */
+import Grid from '@material-ui/core/Grid';
+
+/* Form Components */
 import Form from '~/app/components/form/Form';
 import FormButton from '~/app/components/form/FormButton';
 import InputText from '~/app/components/form/InputText';
 
-export { Form, FormButton, InputText };
+export { Form, FormButton, InputText, Grid };
